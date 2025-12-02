28f987fd4cf1a19eb51fe5b8ee1622d56388d3b5
Giovanni Olavarría <gi.olavarria@duocuc.cl>
2025-12-02 10:18:21 -0300

    backend creado, refactoreado de backend que se tenía anteriormente

--- src/main/java/tande/house/backend/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package tande.house.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tande.house.backend.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
